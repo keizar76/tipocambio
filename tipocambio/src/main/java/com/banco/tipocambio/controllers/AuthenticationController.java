1bea89c8b2f84a0bde8af294d211ddcc3a1f78af
keila.carpio.contractor <keila.carpio.contractor@bbva.com>
2024-02-15 17:45:24 -0500

    exceptions

--- tipocambio/src/main/java/com/banco/tipocambio/controllers/AuthenticationController.java
+++ tipocambio/src/main/java/com/banco/tipocambio/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.banco.tipocambio.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200/"})
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     @PostMapping("/signup")
--- tipocambio/src/main/java/com/banco/tipocambio/controllers/TipoCambioController.java
+++ tipocambio/src/main/java/com/banco/tipocambio/controllers/TipoCambioController.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/v1/tipocambio")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200/"})
 public class TipoCambioController {
     private final TipoCambioServicioImpl tipoCambioServicio;
     
